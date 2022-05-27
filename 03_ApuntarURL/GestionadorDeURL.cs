a4d10c0a6a4f0920903a6e67a6a9b4b1f56ec9b8
Hernan Casado <hcasado@gmail.com>
2022-05-27 17:30:59 -0300

    revision 1

--- 03_ApuntarURL/GestionadorDeURL.cs
+++ 03_ApuntarURL/GestionadorDeURL.cs
@@ -14,22 +14,28 @@ namespace _03_ApuntarURL
     {
         public void CrearTexto(string nombre, string directorioDePC)
         {
-            
-            String fileFinal = System.IO.Path.Combine(directorioDePC, nombre+".txt");
-            System.IO.FileStream fs = System.IO.File.Create(fileFinal);
-            fs.Close();
+            var fileFinal = System.IO.Path.Combine(directorioDePC, nombre+".txt");
+            using (var fs = System.IO.File.Create(fileFinal))
+            {
+                fs.Close();
+            }
         }
+
 
         public void EscribirWeb(string url, string directory, string nombreDelArchivo )
         {
+            //TODO: usar using
             StreamWriter sw = new StreamWriter(directory + "/" + nombreDelArchivo +".txt");
+            
+            //TODO: aprender a usar RestSharp
             HtmlWeb web = new HtmlWeb();
             HtmlDocument html = web.Load(url);
-            var nodes = html.DocumentNode.CssSelect("body").Select(x => x.InnerText);
-            nodes.ToList().ForEach(e =>
+            
+            var nodes = html.DocumentNode.CssSelect("body").Select(x => x.OuterHtml);
+            foreach (var n in nodes)
             {
-                sw.WriteLine(e);
-            });
+                sw.WriteLine(n);
+            }
         }
     }
 }
--- 03_ApuntarURL/Program.cs
+++ 03_ApuntarURL/Program.cs
@@ -11,12 +11,24 @@ namespace _03_ApuntarURL
     {
         private static void Main(string[] args)
         {
-           
+
             GestionadorDeURL gestionador = new GestionadorDeURL();
-            Console.WriteLine("Ingrese la direccion Web");
-            var url = Console.ReadLine();  
+            string url;
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Ingrese la direccion Web");
+                url = Console.ReadLine();
+            }
+
+            //TODO: que la url ingrese por argumento de linea de comandos (args)
             Console.WriteLine("Ingrese el directorio donde guardar");
             var directory = Console.ReadLine();
+
+            //TODO: que la url ingrese por argumento de linea de comandos (args)
             Console.WriteLine("Ingrese el nombre del archivo");
             var nombreDelArchivo = Console.ReadLine();
             
